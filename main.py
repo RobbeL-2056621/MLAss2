569d061825da888d63f804f664ed86d1a3915a08
RobinW-1952879 <71923540+RobinW-1952879@users.noreply.github.com>
2024-05-21 11:50:05 +0200

    Finished application

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
 from joblib import load
-
-
+from keras import saving, layers
+from skimage.transform import resize
+import skimage as ski
+import numpy as np
+categories = ["CombWrench", "Hammer", "Screwdriver", "Wrench"]
 class ImageClassifier:
     def __init__(self, master):
         self.master = master
@@ -33,25 +36,32 @@ class ImageClassifier:
 
 
     def load_image(self):
-        file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg;*.png")])
+        file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg;*.png;*.JPEG")])
         if file_path:
-            self.image = Image.open(file_path)
+            self.image = ski.io.imread(file_path)
             self.load_image_button.config(text="Change Image")
             self.check_compute_button()
 
     def load_model(self):
-        file_path = filedialog.askopenfilename(filetypes=[("ML model", "*.model")])
+        file_path = filedialog.askopenfilename(filetypes=[("ML model", "*.keras")])
         if file_path:
-            self.model = load(file_path)
+            self.model = saving.load_model(file_path)
             self.load_model_button.config(text="Change Model")
             self.check_compute_button()
 
     def compute(self):
         #TODO
-        self.result_label.config(text="Result: Computation performed.")
+        if self.image is not None and self.model is not None:
+            results = self.model.predict(np.asarray([resize(self.image, (300, 300))]))
+            softmax_layer = layers.Softmax()
+            probabilities = np.array(softmax_layer(results))[0]
+            index = np.argmax(probabilities)
+            prediction = categories[index]
+            prob = probabilities[index]
+            self.result_label.config(text=f"Predicted to be {prediction} with probability {prob}.")
 
     def check_compute_button(self):
-        if self.image and self.model:
+        if self.image is not None and self.model is not None:
             self.compute_button.config(state=tk.NORMAL)
         else:
             self.compute_button.config(state=tk.DISABLED)
